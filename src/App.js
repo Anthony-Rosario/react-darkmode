9dce8efb5e0475200fc098cbec47207ded7e2e6d
Anthony Rosario <anthonymrosario225@gmail.com>
2022-03-02 19:24:40 -0800

    initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import useLocalStorage from "use-local-storage";
+import "./App.css";
+
+
 
 function App() {
+  const defaultDark = window.matchMedia("(prefer-color-scheme: dark").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+      <span>Darkmode funsiezzz</span>
+      <button onClick={switchTheme}>
+        Switch Theme {theme === "light" ? "dark" : "light"}
+      </button>
     </div>
   );
 }
